284da487220c584aecfad6fd98479049c9edff51
Екатерина Зверева <eszvereva@activebc.ru>
2019-11-09 13:16:19 +0300

    toxic

--- toxic.py
+++ toxic.py
@@ -1,16 +1,19 @@
 # coding: utf-8
 
 import pickle
-import re, string
+import re
+import string
 
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
-subm = pd.read_csv('sample_submission.csv')
+from collections import namedtuple
+
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+subm = pd.read_csv('data/sample_submission.csv')
 
 label_cols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 train['none'] = 1 - train[label_cols].max(axis=1)
@@ -19,22 +22,25 @@ COMMENT = 'comment_text'
 train[COMMENT].fillna("unknown", inplace=True)
 test[COMMENT].fillna("unknown", inplace=True)
 
-re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
 
-
-def tokenize(s): return re_tok.sub(r' \1 ', s).split()
+def tokenize(s):
+    re_tok = re.compile('([{}“”¨«»®´·º½¾¿¡§£₤‘’])'.format(string.punctuation))
+    return re_tok.sub(r' \1 ', s).split()
 
 
 n = train.shape[0]
 vec = TfidfVectorizer(ngram_range=(1, 2), tokenizer=tokenize,
                       min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
                       smooth_idf=1, sublinear_tf=1)
+
 trn_term_doc = vec.fit_transform(train[COMMENT])
+
 test_term_doc = vec.transform(test[COMMENT])
 
-vocab_filename = f'models/vocabulary.pkl'
-with open(vocab_filename, 'wb') as m_file, open(vocab_filename, 'wb') as r_file:
-    pickle.dump(trn_term_doc, m_file)
+vec_filename = 'models/vectorizer.pkl'
+
+with open(vec_filename, 'wb') as vec_file:
+    pickle.dump(vec, vec_file)
 
 
 def pr(y_i, y):
@@ -57,16 +63,18 @@ def get_mdl(y):
 
 preds = np.zeros((len(test), len(label_cols)))
 
-for i, j in enumerate(label_cols):
-    print('fit', j)
-    m, r = get_mdl(train[j])
-    print(r)
-    preds[:, i] = m.predict_proba(test_x.multiply(r))[:, 1]
-    print(preds)
-    model_filename = f'models/{j}_model.pkl'
-    r_filename = f'models/{j}_r.pkl'
-    with open(model_filename, 'wb') as m_file, open(r_filename, 'wb') as r_file:
-        pickle.dump(m, m_file)
-        pickle.dump(r, r_file)
+Models = namedtuple('models', 'model ratio')
 
 
+def save_models():
+    for i, j in enumerate(label_cols):
+        print('fit', j)
+        m, r = get_mdl(train[j])
+        preds[:, i] = m.predict_proba(test_x.multiply(r))[:, 1]
+        model = Models(model=m, ratio=r)
+        model_filename = f'models/{j}_model.pkl'
+        with open(model_filename, 'wb') as m_file:
+            pickle.dump(model, m_file)
+
+
+save_models()
